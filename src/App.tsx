a70c6dbbcaedd8b21f68c2f793f1392f47fedbb8
Btojaka <bruthtoj@gmail.com>
2025-02-09 23:58:02 +0100

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/Clients/HomeScreen";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,18 +8,20 @@ import AdminScreen from "./screens/Admin/AdminScreen";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/product/:id" element={<ProductDetailScreen />} />
-          <Route path="/cart" element={<CartScreen />} />
-          <Route path="/admin" element={<AdminScreen />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductDetailScreen />} />
+            <Route path="/cart" element={<CartScreen />} />
+            <Route path="/admin" element={<AdminScreen />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
