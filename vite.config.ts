e286ec911285b064d6eb665a4acefd0ab63755d1
Btojaka <bruthtoj@gmail.com>
2025-02-09 16:19:59 +0100

    fix api url ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     "import.meta.env.VITE_API_URL": JSON.stringify(
-      "postgresql://postgres:dQBzoIofBVEtRSvFRFgaYqpmGsjIMmaL@autorack.proxy.rlwy.net:27709/railway"
+      "https://github.com/Btojaka/bicycle-shop-backend"
     ),
   },
   test: {
